9b8c4f86d1b275abf8e0f034385bc965d140bfea
dengyakui <dengyakui@outlook.com>
2019-06-14 17:55:35 +0800

    init

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Login(string authCode)
+        {
+            var baseUrl = "https://oapi.dingtalk.com";
+            // fetch access token
+            var client = new HttpClient();
+            var response =
+                await client.GetStringAsync(
+                    $"{baseUrl}/gettoken?appkey={Constant.APP_KEY}&appsecret={Constant.APP_SECRET}");
+            var resObj = JsonConvert.DeserializeObject<GetAccessTokenResponse>(response);
+            if (resObj.errcode != 0)
+            {
+                return Json(resObj);
+            }
+
+            // fetch user id by access_token
+            var responseStr =
+                await client.GetStringAsync(
+                    $"{baseUrl}/user/getuserinfo?access_token={resObj.access_token}&code={authCode}");
+            var userIdResponse = JsonConvert.DeserializeObject<GetUserIdResponse>(responseStr);
+            if (userIdResponse.errcode != 0)
+            {
+                return Json(userIdResponse);
+            }
+
+            // fetch user info by userId and token
+            var getUserRes = await client.GetStringAsync(
+                $"{baseUrl}/user/get?access_token={resObj.access_token}&userid={userIdResponse.userid}");
+
+            var userInfoRes = JsonConvert.DeserializeObject<UserDetail>(getUserRes);
+            if (userInfoRes.errcode != 0)
+            {
+                return Json(userInfoRes.errmsg);
+            }
+
+
+            return Ok(new
+            {
+                Result = new
+                {
+                    userId = userInfoRes.userid,
+                    userName = userInfoRes.name
+                }
+            });
+
+        }
+    }
+}
---
+++ WebApplication1/Models/Constant.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public class Constant
+    {
+        /**
+         * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppKey
+         */
+        public const String APP_KEY = "dingzmpbosimpev5pra6";
+        /**
+         * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppSecret
+         */
+        public const String APP_SECRET = "bwfem_7QM62NzTg6IcuFRyN3EWUWi-wQu8JWGqkt3JAcCdFQ9n8mHHjbyx4bBrot";
+    }
+
+
+    public class GetAccessTokenResponse
+    {
+        public int errcode { get; set; }
+        public string errmsg { get; set; }
+        public string access_token { get; set; }
+    }
+
+
+    public class GetUserIdResponse
+    {
+        public string userid { get; set; }
+        public int sys_level { get; set; }
+        public string errmsg { get; set; }
+        public bool is_sys { get; set; }
+        public string deviceId { get; set; }
+        public int errcode { get; set; }
+    }
+
+
+    public class UserDetail
+    {
+        public int errcode { get; set; }
+        public string unionid { get; set; }
+        public string remark { get; set; }
+        public string userid { get; set; }
+        public string isLeaderInDepts { get; set; }
+        public bool isBoss { get; set; }
+        public long hiredDate { get; set; }
+        public bool isSenior { get; set; }
+        public string tel { get; set; }
+        public int[] department { get; set; }
+        public string workPlace { get; set; }
+        public string email { get; set; }
+        public string orderInDepts { get; set; }
+        public string mobile { get; set; }
+        public string errmsg { get; set; }
+        public bool active { get; set; }
+        public string avatar { get; set; }
+        public bool isAdmin { get; set; }
+        public bool isHide { get; set; }
+        public string jobnumber { get; set; }
+        public string name { get; set; }
+        public Extattr extattr { get; set; }
+        public string stateCode { get; set; }
+        public string position { get; set; }
+        public Role[] roles { get; set; }
+    }
+
+    public class Extattr
+    {
+    }
+
+    public class Role
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string groupName { get; set; }
+    }
+
+
+
+}
